8e3eda46dccb8f4eddc1aede358b9a7da4ba9cf4
Jovy Cortez <jovycortez11@yahoo.com>
2015-01-27 16:12:07 -0500

    My First Commit

---
+++ src/myGitHubPackage/MyClassOnGitHub.java
@@ -0,0 +1,10 @@
+package myGitHubPackage;
+
+public class MyClassOnGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My First Commit!");
+	}
+}
